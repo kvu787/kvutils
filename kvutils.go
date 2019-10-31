288f8d62921908fbbafe0f0867f34476fa5a87db
Kevin Vu <kevin.m.vu@gmail.com>
2019-10-31 09:38:32 -0700

    Moved from kvu787/log2

---
+++ kvutils.go
@@ -0,0 +1,60 @@
+package kvutils
+
+import (
+	"io"
+	"log"
+	"os"
+	"sync"
+)
+
+type Options struct {
+	UseStdout         bool
+	FilePath          string
+	AdditionalWriters []io.Writer
+}
+
+// Logger can be used concurrently.
+func NewLogger(options Options) (*log.Logger, error) {
+	writers := make([]io.Writer, len(options.AdditionalWriters))
+	copy(writers, options.AdditionalWriters)
+
+	if options.FilePath != "" {
+		file, err := os.Create(options.FilePath)
+		if err != nil {
+			return nil, err
+		}
+		err = file.Close()
+		if err != nil {
+			return nil, err
+		}
+		file, err = os.OpenFile(options.FilePath, os.O_WRONLY, 0)
+		if err != nil {
+			return nil, err
+		}
+		writers = append(writers, file)
+	}
+
+	if options.UseStdout {
+		writers = append(writers, os.Stdout)
+	}
+
+	writer := newSyncWriter(io.MultiWriter(writers...))
+	logger := log.New(writer, "", log.LUTC|log.Ldate|log.Ltime|log.Lshortfile)
+	return logger, nil
+}
+
+type syncWriter struct {
+	mutex  *sync.Mutex
+	writer io.Writer
+}
+
+func newSyncWriter(writer io.Writer) io.Writer {
+	return &syncWriter{&sync.Mutex{}, writer}
+}
+
+func (sw *syncWriter) Write(p []byte) (n int, err error) {
+	sw.mutex.Lock()
+	n, err = sw.writer.Write(p)
+	sw.mutex.Unlock()
+	return n, err
+}
---
+++ kvutils_test.go
@@ -0,0 +1,34 @@
+package kvutils
+
+import (
+	"sync"
+	"testing"
+)
+
+var DefaultOptions Options = Options{true, "test.log", nil}
+
+func TestLog(t *testing.T) {
+	logger, err := NewLogger(DefaultOptions)
+	if err != nil {
+		t.Fatal(err)
+	}
+	logger.Println("here")
+}
+
+func TestLogConcurrent(t *testing.T) {
+	logger, err := NewLogger(DefaultOptions)
+	if err != nil {
+		t.Fatal(err)
+	}
+	logger.Println("here")
+	numThreads := 10
+	wg := &sync.WaitGroup{}
+	wg.Add(numThreads)
+	for i := 0; i < numThreads; i++ {
+		go func(n int) {
+			logger.Println(n)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
