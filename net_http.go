4c8fae62e65d29d5f031485e151317571abcddfa
Kevin Vu <kevin.m.vu@gmail.com>
2019-12-18 04:09:37 -0800

    Add http and websocket helpers

--- net_http.go
+++ net_http.go
@@ -4,33 +4,56 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/rs/cors"
 )
+
+var upgrader = websocket.Upgrader{} // use default options
 
 type JsonRequest struct {
 	Json     interface{}
 	Response http.ResponseWriter
-	Complete chan interface{}
+	complete chan interface{}
 }
 
-func PostJson(host string, port uint, object interface{}) error {
+func (jsonRequest JsonRequest) Complete() {
+	close(jsonRequest.complete)
+}
+
+func (jsonRequest JsonRequest) RespondJson(object interface{}) error {
 	jsonBytes, err := json.Marshal(object)
 	if err != nil {
 		return err
 	}
-
-	_, err = http.Post(fmt.Sprintf("http://%v:%v", host, port), "application/json", bytes.NewReader(jsonBytes))
+	_, err = io.Copy(jsonRequest.Response, bytes.NewReader(jsonBytes))
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
+func PostJson(host string, port uint, object interface{}) (*http.Response, error) {
+	jsonBytes, err := json.Marshal(object)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := http.Post(fmt.Sprintf("http://%v:%v", host, port), "application/json", bytes.NewReader(jsonBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	return response, err
+}
+
+// Starts one goroutine to listen for requests.
+// Starts one goroutine to handle each request. Exits when Complete is called.
+//
 // You can receive requests concurrently.
 // You can complete requests concurrently and in any order.
 func Listen(port uint, logger *log.Logger) chan JsonRequest {
@@ -40,7 +63,13 @@ func Listen(port uint, logger *log.Logger) chan JsonRequest {
 		serveMux := http.NewServeMux()
 		serveMux.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
 			if request.Method != "POST" {
-				logger.Print("Only POST requests are allowed")
+				logger.Printf("Only POST requests are allowed, got %v", request.Method)
+				return
+			}
+
+			contentType := request.Header.Get("Content-Type")
+			if contentType != "application/json; charset=UTF-8" {
+				logger.Print("Content-Type header must be 'application/json; charset=UTF-8', got %v, contentType")
 				return
 			}
 
@@ -58,8 +87,7 @@ func Listen(port uint, logger *log.Logger) chan JsonRequest {
 			}
 
 			complete := make(chan interface{})
-			jsonRequest := JsonRequest{jsonObject, response, complete}
-			requests <- jsonRequest
+			requests <- JsonRequest{jsonObject, response, complete}
 			<-complete
 		})
 
@@ -73,4 +101,46 @@ func Listen(port uint, logger *log.Logger) chan JsonRequest {
 	}()
 
 	return requests
+}
+
+type WebSocket struct {
+	Conn     *websocket.Conn
+	complete chan interface{}
+}
+
+func (webSocket *WebSocket) Complete() {
+	close(webSocket.complete)
+}
+
+// Starts one goroutine to listen for sockets.
+// Starts one goroutine to handle each socket. Exits when Complete is called.
+//
+// You can receive and use sockets concurrently.
+func ListenWebSocket(port uint, logger *log.Logger) chan *WebSocket {
+	connections := make(chan *WebSocket)
+
+	go func() {
+		serveMux := http.NewServeMux()
+		serveMux.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+			conn, err := upgrader.Upgrade(response, request, nil)
+			if err != nil {
+				logger.Print(err)
+				return
+			}
+
+			complete := make(chan interface{})
+			connections <- &WebSocket{conn, complete}
+			<-complete
+		})
+
+		corsHandler := cors.Default().Handler(serveMux)
+		server := http.Server{Addr: fmt.Sprintf(":%v", port), Handler: corsHandler}
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Print(err)
+		}
+		close(connections)
+	}()
+
+	return connections
 }
